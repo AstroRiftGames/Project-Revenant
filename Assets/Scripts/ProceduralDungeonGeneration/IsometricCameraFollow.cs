c524be654e02e3220e5ad3513ef7386845ea1b1e
Tomas Taboada <tomastaboada27@gmail.com>
2026-04-04 16:51:16 -0300

    Update IsometricCameraFollow.cs & added script to MainCamera

--- Assets/Scripts/ProceduralDungeonGeneration/IsometricCameraFollow.cs
+++ Assets/Scripts/ProceduralDungeonGeneration/IsometricCameraFollow.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System.Collections;
 
 namespace ProceduralDungeon
 {
@@ -9,14 +11,50 @@ namespace ProceduralDungeon
         public Vector3 Offset = new Vector3(0f, 0f, -10f);
 
         private Vector3 _velocity = Vector3.zero;
+        private Vector3 _targetPosition = Vector3.zero;
 
-        private void LateUpdate()
+        private void Start()
+        {
+            if (Target == null)
+            {
+                StartCoroutine(FindPlayerRoutine());
+            }
+        }
+
+        private IEnumerator FindPlayerRoutine()
+        {
+            while (Target == null)
+            {
+                GameObject player = GameObject.FindGameObjectWithTag("Player");
+                if (player != null)
+                {
+                    Target = player.transform;
+                    UpdateTargetPos();
+                    transform.position = _targetPosition;
+                }
+                else
+                {
+                    yield return new WaitForSeconds(0.1f);
+                }
+            }
+        }
+
+        private void UpdateTargetPos() 
         {
             if (Target == null) return;
 
-            Vector3 targetPosition = Target.position + Offset;
+            _targetPosition = Target.position + Offset;
+        }
 
-            transform.position = Vector3.SmoothDamp(transform.position, targetPosition, ref _velocity, SmoothTime);
+        private void LateUpdate()
+        {
+            UpdateTargetPos();
+            UpdatePos();
+        }
+
+        private void UpdatePos()
+        {
+            transform.position = Vector3.SmoothDamp(transform.position, _targetPosition, ref _velocity, SmoothTime);
         }
     }
 }
