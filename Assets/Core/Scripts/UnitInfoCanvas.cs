6c1783ac7afdfeaa5ed70241cbaa95b6f2369657
Tomas Taboada <tomastaboada27@gmail.com>
2026-06-03 19:28:36 -0300

    Implement Facade in UnitInfoCanvas

--- Assets/Core/Scripts/UnitInfoCanvas.cs
+++ Assets/Core/Scripts/UnitInfoCanvas.cs
@@ -3,77 +3,29 @@ using UnityEngine.UI;
 
 public class UnitInfoCanvas : MonoBehaviour
 {
-    [SerializeField] private Slider _lifeBar;
-    [SerializeField] private GameObject DamageDealtPrefab;
-    [SerializeField] private UnitHealthBarEffectFeedback _effectFeedback;
-    private LifeController _LC;
+    [SerializeField] private UnitLifeBarUI _lifeBarUI;
+
     private Canvas _canvas;
-    private int _maxHP;
-    [SerializeField] private Color _allyColor = Color.blue;
-    [SerializeField] private Color _enemyColor = Color.red;
 
     private void Awake()
     {
-        TryGetComponent(out Canvas canvas);
-        _canvas = canvas;
-        _LC = GetComponentInParent<LifeController>();
+        TryGetComponent(out _canvas);
     }
 
     private void Start()
     {
-        _canvas.worldCamera = Camera.main;
-        _maxHP = _LC.MaxHealth;
-        
-        UnitAffiliationState affiliation = GetComponentInParent<UnitAffiliationState>();
-        bool isAlly = affiliation != null && affiliation.Team == UnitTeam.Ally;
-        _lifeBar.fillRect.GetComponent<Image>().color = isAlly ? _allyColor : _enemyColor;
-
-        _lifeBar.maxValue = _maxHP;
-        UpdateLifeBar(_maxHP);
-
-        InitializeEffectFeedback();
-    }
-
-    private void InitializeEffectFeedback()
-    {
-        if (_effectFeedback == null)
+        if (_canvas != null)
         {
-            Debug.LogWarning($"[{nameof(UnitInfoCanvas)}] EffectFeedback reference not assigned on {gameObject.name}. Visual feedback will not appear.", this);
-            return;
+            _canvas.worldCamera = Camera.main;
         }
 
-        StatusEffectController statusEffectController = GetComponentInParent<StatusEffectController>();
-        if (statusEffectController == null)
+        if (_lifeBarUI != null)
         {
-            Debug.LogWarning($"[{nameof(UnitInfoCanvas)}] No StatusEffectController found in parent of {gameObject.name}. Cannot initialize effect feedback.", this);
-            return;
+            LifeController lifeController = GetComponentInParent<LifeController>();
+            UnitAffiliationState affiliation = GetComponentInParent<UnitAffiliationState>();
+            StatusEffectController statusEffectController = GetComponentInParent<StatusEffectController>();
+
+            _lifeBarUI.Initialize(lifeController, affiliation, statusEffectController, _canvas);
         }
-
-        _effectFeedback.Initialize(statusEffectController);
-    }
-
-    private void OnEnable()
-    {
-        _LC.OnLifeUpdated += UpdateLifeBar;
-        _LC.OnDamageTaken += ShowDamageTaken;
-    }
-
-    private void OnDisable()
-    {
-        _LC.OnLifeUpdated -= UpdateLifeBar;
-        _LC.OnDamageTaken -= ShowDamageTaken;
-    }
-
-    private void UpdateLifeBar(int newHP)
-    {
-        _lifeBar.value = newHP;
-    }
-
-    private void ShowDamageTaken(int damage)
-    {
-        Instantiate(DamageDealtPrefab, transform.up * .5f, Quaternion.identity).TryGetComponent(out HealthDeltaText lifeUpdate);
-        HealthDeltaText newText = lifeUpdate;
-        newText.transform.SetParent(_canvas.transform, true);
-        newText.Play(-damage);
     }
 }
---
+++ Assets/Core/Scripts/UnitLifeBarUI.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitLifeBarUI : MonoBehaviour
+{
+    [SerializeField] private Slider _lifeBar;
+    [SerializeField] private GameObject _damageDealtPrefab;
+    [SerializeField] private UnitHealthBarEffectFeedback _effectFeedback;
+    [SerializeField] private Color _allyColor = Color.blue;
+    [SerializeField] private Color _enemyColor = Color.red;
+
+    private LifeController _lifeController;
+    private Canvas _parentCanvas;
+    private int _maxHP;
+
+    public void Initialize(LifeController lifeController, UnitAffiliationState affiliation, StatusEffectController statusEffectController, Canvas parentCanvas)
+    {
+        _lifeController = lifeController;
+        _parentCanvas = parentCanvas;
+
+        if (_lifeController != null)
+        {
+            _maxHP = _lifeController.MaxHealth;
+            _lifeBar.maxValue = _maxHP;
+            UpdateLifeBar(_maxHP);
+
+            _lifeController.OnLifeUpdated += UpdateLifeBar;
+            _lifeController.OnDamageTaken += ShowDamageTaken;
+        }
+
+        bool isAlly = affiliation != null && affiliation.Team == UnitTeam.Ally;
+        if (_lifeBar != null && _lifeBar.fillRect != null && _lifeBar.fillRect.TryGetComponent(out Image fillImage))
+        {
+            fillImage.color = isAlly ? _allyColor : _enemyColor;
+        }
+
+        if (_effectFeedback != null)
+        {
+            if (statusEffectController != null)
+            {
+                _effectFeedback.Initialize(statusEffectController);
+            }
+            else
+            {
+                Debug.LogWarning($"[{nameof(UnitLifeBarUI)}] No StatusEffectController provided. Cannot initialize effect feedback.", this);
+            }
+        }
+        else
+        {
+            Debug.LogWarning($"[{nameof(UnitLifeBarUI)}] EffectFeedback reference not assigned on {gameObject.name}. Visual feedback will not appear.", this);
+        }
+    }
+
+    private void OnDestroy()
+    {
+        if (_lifeController != null)
+        {
+            _lifeController.OnLifeUpdated -= UpdateLifeBar;
+            _lifeController.OnDamageTaken -= ShowDamageTaken;
+        }
+    }
+
+    private void UpdateLifeBar(int newHP)
+    {
+        if (_lifeBar != null)
+        {
+            _lifeBar.value = newHP;
+        }
+    }
+
+    private void ShowDamageTaken(int damage)
+    {
+        if (_damageDealtPrefab == null || _parentCanvas == null) return;
+
+        Instantiate(_damageDealtPrefab, transform.up * .5f, Quaternion.identity).TryGetComponent(out HealthDeltaText lifeUpdate);
+        if (lifeUpdate != null)
+        {
+            lifeUpdate.transform.SetParent(_parentCanvas.transform, true);
+            lifeUpdate.Play(-damage);
+        }
+    }
+}
